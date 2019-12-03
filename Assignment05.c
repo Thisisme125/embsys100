c751b1b6b13d77ed108d43f5ce678e0e4152ad00
Thisisme125 <thisisme_online@hotmail.com>
2019-12-02 16:34:33 -0800

    Fixed swap to work on pointers

--- Assignment05.c
+++ Assignment05.c
@@ -1,7 +1,14 @@
 void swap(int* x, int* y)
-
 {
- int temp = *x;
- *x = *y;
- *y = temp;
+ int* temp = x;
+ x = y;
+ y = temp;
+}
+
+int main(void)
+{
+ int x = 1;
+ int y = 2;
+ 
+ swap(&x, &y);
 }
