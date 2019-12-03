a22abc5a39151df75d54ded1a4ee08f02d07ead3
Thisisme125 <thisisme_online@hotmail.com>
2019-12-02 19:53:28 -0800

    Update main.c

--- assignment05/swap/main.c
+++ assignment05/swap/main.c
@@ -1,5 +1,16 @@
-
-int main()
+  
+void swap(int* x, int* y)
 {
-  return 0;
+ int* temp = x;
+ x = y;
+ y = temp;
+}
+
+int main(void)
+{
+ int x = 1;
+ int y = 2;
+ swap(&x, &y);
+  
+ return 0;
 }
